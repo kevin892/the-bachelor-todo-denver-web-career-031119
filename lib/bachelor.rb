6d17a0b7556df778ddcb693a51878588fba01c17
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-11 23:19:42 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -27,6 +27,7 @@ def count_contestants_by_hometown(data, hometown)
   data.each do |seasons, arrays|
     arrays.each do |x|
       return x["name"] if x["age"] == 27
+      binding.pry
       end
     end
     end
