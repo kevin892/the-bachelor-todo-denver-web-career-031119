5dddf67beacd8883eb7eb29fbaf37f5bb13cfc28
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-11 23:58:32 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -42,7 +42,7 @@ def get_average_age_for_season(data, season)
   data.each do |seasons, arrays|
     j = arrays.length
     if season == seasons
-          arrays.each do |x|
+    arrays.each do |x|
       counter += x["age"].to_i
 end
 end
