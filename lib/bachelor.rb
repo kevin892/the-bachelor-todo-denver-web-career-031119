1c16b9f44314911fa9f6416d7bc0f06b357ee4e6
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-12 00:22:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,8 +1,8 @@
 require "pry"
 def get_first_name_of_season_winner(data, season)
-   data.each do |seasons, arrays|
+   data.each do |seasons, array|
    if season == seasons
-   arrays.each do |x|
+   array.each do |x|
        return x["name"].split(' ').first if x["status"]= "Winner"
       end
     end
@@ -11,8 +11,8 @@ end
 
 
 def get_contestant_name(data, occupation)
-  data.each do |seasons, arrays|
-    arrays.each do |x|
+  data.each do |seasons, array|
+    array.each do |x|
     return x["name"] if x["occupation"] == occupation
     end
   end
@@ -20,8 +20,8 @@ end
 
 def count_contestants_by_hometown(data, hometown)
   counter=0
-  data.each do |seasons, arrays|
-    arrays.each do |x|
+  data.each do |seasons, array|
+    array.each do |x|
       counter += 1 if x["hometown"] == hometown
         end
       end
@@ -30,8 +30,8 @@ end
 
 
 def get_occupation(data, hometown)
-  data.each do |seasons, arrays|
-    arrays.each do |x|
+  data.each do |seasons, array|
+    array.each do |x|
       return x["occupation"] if x["hometown"] == hometown
     end
   end
@@ -39,10 +39,10 @@ end
 
 def get_average_age_for_season(data, season)
   counter = 0
-  data.each do |seasons, arrays|
+  data.each do |seasons, array|
     if season == seasons
-      num = arrays.length
-    arrays.each do |x|
+      num = array.length
+    array.each do |x|
       counter += x["age"].to_i
 end
 counter /= num.to_f
